7e3d6d7ed4b2ef974afb868ebd6efeba5d67b81f
Shamim <mdshamim723@gmail.com>
2023-05-28 23:25:38 +0600

    solved

---
+++ src/main/java/n28_find_index_of_the_first_occurrence_in_a_string/n2_add_two_numbers/Solution.java
@@ -0,0 +1,9 @@
+package n28_find_index_of_the_first_occurrence_in_a_string.n2_add_two_numbers;
+
+class Solution {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
