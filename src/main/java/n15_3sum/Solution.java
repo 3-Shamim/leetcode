c98d9a1f9d0be2e64a6c6e8e2e9844dc7e859fc7
Shamim <shamim.molla@vivasoftltd.com>
2023-08-21 17:35:28 +0600

    need to be solved

--- src/main/java/n15_3sum/Solution.java
+++ src/main/java/n15_3sum/Solution.java
@@ -12,14 +12,26 @@ class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
 
-        for (int i = 0; i < nums.length - 3; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
 
             for (int j = i; j < nums.length - 2; j++) {
 
-                System.out.print(nums[j]);
-                System.out.print(nums[j + 1]);
-                System.out.print(nums[j + 2]);
-                System.out.println();
+                System.out.printf("Actual %d, %d, %d\n", nums[i], nums[j + 1], nums[j + 2]);
+
+//                int sum = (nums[i] + nums[j + 1] + nums[j + 2]);
+//
+//                System.out.println(sum);
+
+//                if (sum == 0) {
+//
+//                    int low = Math.min(Math.min(nums[i], nums[j + 1]), nums[j + 2]);
+//                    int high = Math.max(Math.max(nums[i], nums[j + 1]), nums[j + 2]);
+//
+//                    int mid = sum - (low + high);
+//
+//                    System.out.printf("Sorted %d, %d, %d\n", low, mid, high);
+//
+//                }
 
             }
 
