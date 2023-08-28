fadcd53fc5c6c40d98010d5799fd7e63ad04e507
Shamim <shamim.molla@vivasoftltd.com>
2023-08-28 17:01:12 +0600

    need to solve n16

---
+++ src/main/java/n17_letter_combinations_of_a_phone_number/Main.java
@@ -0,0 +1,21 @@
+package n17_letter_combinations_of_a_phone_number;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim Molla
+ * Email: shamim.molla@vivasoftltd.com
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.letterCombinations("23"));
+
+        System.out.println(solution.letterCombinations(""));
+
+    }
+
+}
---
+++ src/main/java/n17_letter_combinations_of_a_phone_number/Solution.java
@@ -0,0 +1,33 @@
+package n17_letter_combinations_of_a_phone_number;
+
+import java.util.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim Molla
+ * Email: shamim.molla@vivasoftltd.com
+ */
+
+class Solution {
+
+    private final static Map<Integer, String> STRING_MAP = new HashMap<>();
+
+    static {
+        STRING_MAP.put(2, "abc");
+        STRING_MAP.put(3, "def");
+        STRING_MAP.put(4, "ghi");
+        STRING_MAP.put(5, "jkl");
+        STRING_MAP.put(6, "mno");
+        STRING_MAP.put(7, "pqrs");
+        STRING_MAP.put(8, "tuv");
+        STRING_MAP.put(9, "wxyz");
+    }
+
+    public List<String> letterCombinations(String digits) {
+
+
+
+        return null;
+    }
+
+}
