1039b8d026fc91828ae31b9f3eff8758fa60d458
Shamim <mdshamim723@gmail.com>
2023-09-07 20:54:52 +0600

    solved n88_merge_sorted_array

--- src/main/java/n88_merge_sorted_array/Main.java
+++ src/main/java/n88_merge_sorted_array/Main.java
@@ -55,6 +55,33 @@ class Main {
         solution.merge(nums1, m, nums2, n);
         System.out.println(Arrays.toString(nums1));
 
+        m = 4;
+        n = 5;
+
+        nums1 = new int[]{1, 2, 3, 5, 0, 0, 0, 0, 0};
+        nums2 = new int[]{2, 3, 4, 5, 6};
+
+        solution.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+
+        m = 8;
+        n = 3;
+
+        nums1 = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 0, 0, 0};
+        nums2 = new int[]{4, 5, 6};
+
+        solution.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+
+        m = 4;
+        n = 3;
+
+        nums1 = new int[]{1, 2, 3, 5, 0, 0, 0};
+        nums2 = new int[]{7, 8, 9};
+
+        solution.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+
     }
 
 }
