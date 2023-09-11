992fa31e465a146cd78c178d1d76047ef0d496fa
Shamim <shamim.molla@vivasoftltd.com>
2023-09-11 19:05:02 +0600

    need to solved

--- src/main/java/n169_majority_element/Solution.java
+++ src/main/java/n169_majority_element/Solution.java
@@ -1,11 +1,30 @@
 package n169_majority_element;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
 
     public int majorityElement(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int element = nums[0];
+        int startPoint = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] != nums[i - 1]) {
+
+            }
+
+        }
+
+        return element;
+    }
+
+    public int majorityElement1(int[] nums) {
 
         Map<Integer, Integer> map = new HashMap<>();
         int max = Integer.MIN_VALUE;
