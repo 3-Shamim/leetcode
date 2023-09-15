37f1bcc8c40ae17766e64cab5ec15fde0e8dbc4e
Shamim <mdshamim723@gmail.com>
2023-09-15 20:56:58 +0600

    solved n55_jump_game

--- src/main/java/n55_jump_game/Main.java
+++ src/main/java/n55_jump_game/Main.java
@@ -21,6 +21,10 @@ class Main {
         System.out.println(solution.canJump(new int[]{3, 2, 2, 0, 4, 3, 2, 1, 0}));
         System.out.println(solution.canJump(new int[]{3, 2, 2, 0, 4, 3, 2, 1, 0, 2}));
         System.out.println(solution.canJump(new int[]{1, 1, 1, 1, 0}));
+        System.out.println(solution.canJump(new int[]{1, 3, 1, 1, 4}));
+        System.out.println(solution.canJump(new int[]{3, 2, 0, 1, 4}));
+        System.out.println(solution.canJump(new int[]{5, 0, 0, 6, 0, 0, 4, 0, 0, 0, 4}));
 
     }
+
 }
--- src/main/java/n55_jump_game/Solution.java
+++ src/main/java/n55_jump_game/Solution.java
@@ -1,5 +1,7 @@
 package n55_jump_game;
 
+import java.util.Arrays;
+
 class Solution {
 
     public boolean canJump(int[] nums) {
@@ -23,6 +25,73 @@ class Solution {
 
         return max >= nums.length - 1;
 
+    }
+
+    public boolean canJump1(int[] nums) {
+
+        if (nums.length == 1) {
+            return true;
+        } else if (nums[0] == 0) {
+            return false;
+        }
+
+        boolean match = Arrays.stream(nums).anyMatch(v -> v == 0);
+
+        if (!match) {
+            return true;
+        }
+
+        int[] visited = new int[nums.length];
+
+        return recursion(nums, 0, visited);
+    }
+
+    private boolean recursion(int[] nums, int i, int[] visited) {
+
+        if (i >= nums.length - 1) {
+            return true;
+        }
+
+        if (visited[i] == 1 || nums[i] == 0) {
+            return false;
+        }
+
+        visited[i] = 1;
+
+        for (int j = nums[i]; j > 0; j--) {
+
+            boolean recursion = recursion(nums, i + j, visited);
+
+            if (recursion) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    private boolean recursion1(int[] nums, int i) {
+
+        if (i == nums.length - 1) {
+            return true;
+        }
+
+        if (nums[i] == 0) {
+            return false;
+        }
+
+        for (int j = 1; j <= nums[i]; j++) {
+
+            boolean recursion = recursion1(nums, i + j);
+
+            if (recursion) {
+                return true;
+            }
+
+        }
+
+        return false;
     }
 
 }
