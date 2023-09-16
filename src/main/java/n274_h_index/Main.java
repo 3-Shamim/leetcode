f0654bcc6af11c77abdecccb64ecef6285a24d92
Shamim <mdshamim723@gmail.com>
2023-09-17 00:16:47 +0600

    need to solve n274

---
+++ src/main/java/n274_h_index/Main.java
@@ -0,0 +1,18 @@
+package n274_h_index;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim Molla
+ * Email: shamim.molla@vivasoftltd.com
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+
+    }
+
+}
---
+++ src/main/java/n274_h_index/Solution.java
@@ -0,0 +1,14 @@
+package n274_h_index;
+
+import java.util.Arrays;
+
+class Solution {
+
+    public int hIndex(int[] citations) {
+
+        Arrays.sort(citations);
+
+        return 0;
+    }
+
+}
