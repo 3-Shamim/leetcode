54eaeb9c541ed54b3ad10ea7858eabe44cd47995
Md Shamim <mdshamim723@gmail.com>
2024-07-16 21:39:02 +0600

    solved n436_find_right_interval

--- src/main/java/n436_find_right_interval/No436.java
+++ src/main/java/n436_find_right_interval/No436.java
@@ -3,6 +3,7 @@ package n436_find_right_interval;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,11 +13,12 @@ import java.util.Map;
 
 class No436 {
 
-    public static int[] findRightInterval(int[][] intervals) {
+    // Using builtin tree map
+    public static int[] findRightIntervalTM(int[][] intervals) {
 
         int[] ans = new int[intervals.length];
 
-        Map<Integer, Integer> map = new HashMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -37,7 +39,7 @@ class No436 {
 
         for (int i = 0; i < intervals.length; i++) {
 
-            int endI  = intervals[i][1];
+            int endI = intervals[i][1];
 
             if (endI > max) {
                 ans[i] = -1;
@@ -49,31 +51,87 @@ class No436 {
                 continue;
             }
 
-            binarySearch(endI, max, map, endI);
+            Integer key = map.ceilingKey(endI);
+
+            ans[i] = map.get(key);
 
         }
 
         return ans;
     }
 
-    private static int binarySearch(int start, int end, Map<Integer, Integer> map, int target) {
+    // Using linear inside binary search O(nlog(n))
+    public static int[] findRightInterval(int[][] intervals) {
+
+        int[] ans = new int[intervals.length];
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int[] allStart = new int[intervals.length];
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < intervals.length; i++) {
+
+            if (intervals[i][0] > max) {
+                max = intervals[i][0];
+            }
+
+            if (intervals[i][0] < min) {
+                min = intervals[i][0];
+            }
+
+            map.put(intervals[i][0], i);
+            allStart[i] = intervals[i][0];
+
+        }
+
+        Arrays.sort(allStart);
+
+        for (int i = 0; i < intervals.length; i++) {
+
+            int endI = intervals[i][1];
+
+            if (endI > max) {
+                ans[i] = -1;
+                continue;
+            }
+
+            if (map.containsKey(endI)) {
+                ans[i] = map.get(endI);
+                continue;
+            }
+
+            int value = binarySearch(0, allStart.length, allStart, endI);
+
+            ans[i] = map.get(value);
+
+        }
+
+        return ans;
+    }
+
+    private static int binarySearch(int start, int end, int[] allStart, int target) {
 
         while (start <= end) {
 
             int mid = start + (end - start) / 2;
 
-            if (target > mid) {
+            if (target == allStart[mid]) {
+                return mid;
+            } else if (target > allStart[mid]) {
                 start = mid + 1;
             } else {
-
-                end = mid;
-            } else
+                end = mid - 1;
+            }
 
         }
 
-        return start;
+        return allStart[start];
     }
 
+    // Bruteforce O(n^2)
     public static int[] findRightIntervalBF(int[][] intervals) {
 
         int[] ans = new int[intervals.length];
@@ -107,6 +165,7 @@ class No436 {
 
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(findRightInterval(new int[][]{{4, 5}, {2, 3}, {1, 2}})));
         System.out.println(Arrays.toString(findRightInterval(new int[][]{{1, 2}})));
         System.out.println(Arrays.toString(findRightInterval(new int[][]{{3, 4}, {2, 3}, {1, 2}})));
         System.out.println(Arrays.toString(findRightInterval(new int[][]{{1, 4}, {2, 3}, {3, 4}})));
